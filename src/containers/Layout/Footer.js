14f48f5b952dfbcc6d25e984182cd0beb9a605bf
Mahla <107788139+MahlaParvaz@users.noreply.github.com>
2024-02-24 17:22:23 +0330

    edit footer

--- src/containers/Layout/Footer.js
+++ src/containers/Layout/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bottom-0 left-0 right-0 w-full py-1 bg-gray-100">
       <p className="text-gray-700 flex items-center justify-center" dir="ltr">
-        Made with <HeartIcon className="w-6 h-6 fill-red-500 mx-1" /> by Saheb
+        <HeartIcon className="w-6 h-6 fill-red-500 mx-1" />
       </p>
     </footer>
   );
